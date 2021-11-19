888d721c8b10c2c0a42d545ca95d9864114d5d9e
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-11-19 14:00:33 -0300

    fix-teste-login

--- src/test/java/com/alura/appium/FeatureCadastro.java
+++ src/test/java/com/alura/appium/FeatureCadastro.java
@@ -5,16 +5,17 @@ import static org.junit.Assert.assertTrue;
 import io.appium.java_client.MobileElement;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.NoSuchElementException;
 
 import java.net.MalformedURLException;
 
-public class FeatureCadastro
-{
+public class FeatureCadastro {
+    private String prefixo = "br.com.alura.aluraesporte:id/";
+
     @Test
     public void nao_consigo_cadastrar_usuario_com_senhas_que_nao_conferem() throws MalformedURLException {
         AppiumDriverConfig driver = new AppiumDriverConfig();
 
-        String prefixo = "br.com.alura.aluraesporte:id/";
         MobileElement botaoCadastro = (MobileElement)driver.driver.findElementById(prefixo+"login_botao_cadastrar_usuario");
         botaoCadastro.click();
 
@@ -31,5 +32,25 @@ public class FeatureCadastro
         MobileElement msgErro = (MobileElement)driver.driver.findElementById(prefixo+"erro_cadastro");
 
         Assert.assertEquals("Senhas não conferem", msgErro.getText());
+    }
+
+    @Test
+    public void posso_cadastrar_usuario_com_senhas_que_conferem() throws NoSuchElementException, MalformedURLException {
+        AppiumDriverConfig driver = new AppiumDriverConfig();
+
+        MobileElement botaoCadastro = (MobileElement)driver.driver.findElementById(prefixo+"login_botao_cadastrar_usuario");
+        botaoCadastro.click();
+
+        MobileElement campoNome = (MobileElement) driver.driver.findElementById(prefixo+"input_nome");
+        MobileElement campoSenha = (MobileElement) driver.driver.findElementById(prefixo+"input_senha");
+        MobileElement campoConfirmarSenha = (MobileElement)driver.driver.findElementById(prefixo+"input_confirmar_senha");
+        MobileElement botaoConfirmarCadastro = (MobileElement) driver.driver.findElementById(prefixo+"cadastro_usuario_botao_cadastrar");
+
+        campoNome.setValue("Otavio");
+        campoSenha.setValue("123");
+        campoConfirmarSenha.setValue("123");
+
+        botaoConfirmarCadastro.click();
+        MobileElement botaoLogar = (MobileElement) driver.driver.findElementById(prefixo+"login_botao_logar");
     }
 }
