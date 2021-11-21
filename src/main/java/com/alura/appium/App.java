099b06c4bb00d63bfd9a919e7a58a68030d85ab2
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-11-21 20:18:33 -0300

    update-App.java

--- src/main/java/com/alura/appium/App.java
+++ src/main/java/com/alura/appium/App.java
@@ -9,10 +9,6 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) throws MalformedURLException {
